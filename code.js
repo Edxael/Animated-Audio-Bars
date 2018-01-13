67da7c9f187184d8ec26832e539e1fab6841e84f
Edxael <zerphyr.r@gmail.com>
2018-01-13 00:56:09 -0700

    add frameLooper()

--- code.js
+++ code.js
@@ -31,7 +31,7 @@ function initMp3Player() {
   document.getElementById("audio_box").appendChild(audio)
   contex = new webkitAudioContext()  // AudioContext object instance
   analyser = context.createAnalyser()  // analyserNode Method.
-  canvas = document.getElementById("equalizer")
+  canvas = document.getElementById("analyzer_render")
   ctx = cnavas.getContext('2d')
 
   // Re-Route audio playback into the processing graph
@@ -39,4 +39,26 @@ function initMp3Player() {
   source.connect(analyser)
   analyser.connect(context.destination)
   frameLooper()
+}
+
+
+
+  //  This frameLooper() function animates any style of graphics you wish to the audio frequency
+  //  looping all the default frame rate that the browser provides(aprox 60 FPS)
+function frameLooper(){
+  window.webkitRequestAnimationFrame(frameLooper)
+  fbc_array = new Unit8Array(analyser.frequencyBinCount)
+  analyser.getByteFrequencyData(fbc_array)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)  // Clear the canvas
+  ctx.fillStyle = 'rgb(182, 144, 244)'  // Color of the Barss
+  bars = 100
+
+  for(let i = 0; i < bars; i++){
+    bar_x = i * 3
+    bar_width = 2
+    bar_height = -(fbc_array[i] / 2)
+    // fillRect(x, y, width, height) // Explanation of the parameters below
+    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height)
+  }
+
 }
